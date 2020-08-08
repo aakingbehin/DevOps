e65d523835ea4c764a43e2ce36b990a97b1b07cf
aakingbehin <akin@asaconsultingco.com>
2020-08-08 17:21:17 -0400

    two

--- Product.java
+++ Product.java
@@ -1,5 +1,13 @@
-class Product{
 
+class Product{
+	
+	byte bl = -128;	byte bh = 127;
+	short sl = -32768;	short sh = 32767;
+	int il = -2147483648;	int ih = 2147483647;
+	
+	public static void main(String[] args){
+		
+	}
 }
 
 
