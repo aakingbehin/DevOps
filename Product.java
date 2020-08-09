3b72880ba6c215f96dc2deb97eeaede7f471590a
aakingbehin <akin@asaconsultingco.com>
2020-08-09 13:34:49 -0400

    Source file

--- Product.java
+++ Product.java
@@ -1,10 +1,5 @@
-
 class Product{
-	
-	byte bl = -128;	byte bh = 127;
-	short sl = -32768;	short sh = 32767;
-	int il = -2147483648;	int ih = 2147483647;
-	
+
 	public static void main(String[] args){
 		
 	}
