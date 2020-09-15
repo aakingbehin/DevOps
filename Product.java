19db16879f12d5acf04a709edacd793265577599
aakingbehin <akin@asaconsultingco.com>
2020-09-15 15:23:16 -0400

    latest file

--- Product.java
+++ Product.java
@@ -1,3 +1,24 @@
+import java.io.*;
+import java.io.File;
 class Product{
 
+	public static void main(String[] args){
+		try{
+		System.out.println("start of program");
+		//throw new ArrayIndexOutOfBoundsException();
+		//System.out.println(args[0]);
+		File nf = new File("/Users/aakingbehin/Documents/aakingbehin/Research/Java/cmdfiles/Books.java");
+		nf.createNewFile();
+		
+		System.out.println("after the exception is thrown");
+		} 
+		catch(ArrayIndexOutOfBoundsException e){
+			System.out.println("executing the ArrayIndexOutOfBoundsException catch block");
+		}
+		catch(IOException e){
+			System.out.println("executing the ArrayIndexOutOfBoundsException catch block");
+		}
+		System.out.println("after the catch block");
+	}
+	
 }
